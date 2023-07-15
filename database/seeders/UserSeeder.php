be96b5adb42130bd19a576353859802700ee7db6
Johan Prianto Manik Ambarita <johan.prianto.manik.a@corp.bri.co.id>
2023-07-15 09:20:26 +0700

    UserSeeder done and tested (40data)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -15,144 +16,172 @@ class UserSeeder extends Seeder
      * @return void
      */
     public function run()
-    {   
+    {
         Schema::disableForeignKeyConstraints();
         User::truncate();
         Schema::enableForeignKeyConstraints();
 
-         User::create([
-             'full_name' => 'Admin',
-             'student_id_number' => '9871254321',
-             'entry_year' => '2019',
-             'student_major' => 'Information Systems',
-             'username' => 'admin',
-             'email' => 'admin@mail.com',
-             'role' => 'admin',
-             'password' => bcrypt('password')
-         ]);
+        $fullNames = [
+            'John Doe',
+            'Jane Smith',
+            'Michael Brown',
+            'Sophia Anderson',
+            'David Johnson',
+            'Sarah Williams',
+            'Emily Davis',
+            'James Miller',
+            'Olivia Wilson',
+            'Ethan Taylor',
+            'Alex Turner',
+            'Isabella Campbell',
+            'William White',
+            'Mia Thompson',
+            'Benjamin Harris',
+            'Ava Davis',
+            'Jacob Robinson',
+            'Emma Clark',
+            'Daniel Martinez',
+            'Sophie Lewis',
+            'Matthew Hall',
+            'Chloe Baker',
+            'Alexander Green',
+            'Oliver Young',
+            'Grace Mitchell',
+            'Logan Lee',
+            'Abigail Walker',
+            'Ryan Hill',
+            'Emily Stewart',
+            'Jackson Turner',
+            'Elizabeth Adams',
+            'Lucas Carter',
+            'Charlotte Murphy',
+            'Gabriel Nelson',
+            'Lily King',
+            'Samuel Rivera',
+            'Ella Cooper',
+            'Henry Ward',
+            'Victoria Perry',
+            'Christopher Bell',
+            'Scarlett Phillips',
+            'Andrew Butler',
+            'Aria Wright'
+        ];
 
-         User::create([
-            'full_name' => 'Lucky Marbun',
-            'student_id_number' => '987554321',
-            'entry_year' => '2016',
-            'student_major' => 'Computer Science',
-            'username' => 'luckymarbun',
-            'email' => 'luckymarbun@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+        $studentMajorOptions = [
+            'Computer Science',
+            'Electrical Engineering',
+            'Bioprocess Engineering',
+            'Mechanical Engineering'
+        ];
 
-         User::create([
-            'full_name' => 'Jane Smith',
-            'student_id_number' => '987654321',
-            'entry_year' => '2019',
-            'student_major' => 'Information Systems',
-            'username' => 'janesmith',
-            'email' => 'janesmith@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+        $entryYears = [2015, 2016, 2017, 2018];
+        $studentMajorIndex = 0;
 
-        User::create([
-            'full_name' => 'John Doe',
-            'student_id_number' => '123456789',
-            'entry_year' => '2020',
-            'student_major' => 'Computer Science',
-            'username' => 'johndoe',
-            'email' => 'johndoe@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
-        
-        User::create([
-            'full_name' => 'David Johnson',
-            'student_id_number' => '456789123',
-            'entry_year' => '2021',
-            'student_major' => 'Electrical Engineering',
-            'username' => 'davidjohnson',
-            'email' => 'davidjohnson@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
-        
-        User::create([
-            'full_name' => 'Sarah Williams',
-            'student_id_number' => '789123456',
-            'entry_year' => '2018',
-            'student_major' => 'Business Management',
-            'username' => 'sarahwilliams',
-            'email' => 'sarahwilliams@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+        for ($i = 0; $i < 40; $i++) {
+            $fullName = $this->generateUniqueFullName($i, $fullNames);
+            $studentIdNumber = $this->generateUniqueStudentIdNumber();
+            $entryYear = $this->generateEntryYear($i, $entryYears);
+            $studentMajor = $this->generateStudentMajor($studentMajorIndex, $studentMajorOptions);
+            $username = $this->generateUsername($fullName);
+            $email = $this->generateEmail($username);
+            $role = 'user';
+            $password = bcrypt('password');
 
-        User::create([
-            'full_name' => 'Michael Brown',
-            'student_id_number' => '321654987',
-            'entry_year' => '2022',
-            'student_major' => 'Graphic Design',
-            'username' => 'michaelbrown',
-            'email' => 'michaelbrown@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
-        
-        User::create([
-            'full_name' => 'Emily Davis',
-            'student_id_number' => '654789321',
-            'entry_year' => '2017',
-            'student_major' => 'English Language',
-            'username' => 'emilydavis',
-            'email' => 'emilydavis@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+            User::create([
+                'full_name' => $fullName,
+                'student_id_number' => $studentIdNumber,
+                'entry_year' => $entryYear,
+                'student_major' => $studentMajor,
+                'username' => $username,
+                'email' => $email,
+                'role' => $role,
+                'password' => $password
+            ]);
 
-        User::create([
-            'full_name' => 'James Miller',
-            'student_id_number' => '987321654',
-            'entry_year' => '2023',
-            'student_major' => 'Finance',
-            'username' => 'jamesmiller',
-            'email' => 'jamesmiller@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
-        
-        User::create([
-            'full_name' => 'Olivia Wilson',
-            'student_id_number' => '654123987',
-            'entry_year' => '2020',
-            'student_major' => 'Psychology',
-            'username' => 'oliviawilson',
-            'email' => 'oliviawilson@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+            $studentMajorIndex++;
+            if ($studentMajorIndex >= count($studentMajorOptions)) {
+                $studentMajorIndex = 0;
+            }
+        }
+    }
 
-        User::create([
-            'full_name' => 'Ethan Taylor',
-            'student_id_number' => '321987654',
-            'entry_year' => '2021',
-            'student_major' => 'Mass Communication',
-            'username' => 'ethantaylor',
-            'email' => 'ethantaylor@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
-        
-        User::create([
-            'full_name' => 'Sophia Anderson',
-            'student_id_number' => '789654321',
-            'entry_year' => '2019',
-            'student_major' => 'Civil Engineering',
-            'username' => 'sophiaanderson',
-            'email' => 'sophiaanderson@example.com',
-            'role' => 'user',
-            'password' => bcrypt('password')
-        ]);
+        /**
+ * Generate a unique full name based on the given index and array of names.
+ *
+ * @param int $index
+ * @param array $names
+ * @return string
+ */
+private function generateUniqueFullName(int $index, array $names): string
+{
+    $fullNameIndex = $index % count($names);
+    $fullName = $names[$fullNameIndex];
+    return $fullName;
+}
+    
 
-            
-        
+    /**
+     * Generate a unique student ID number.
+     *
+     * @return int
+     */
+    private function generateUniqueStudentIdNumber(): int
+    {
+        $studentIdNumber = rand(100000000, 999999999);
+        return $studentIdNumber;
+    }
+
+    /**
+     * Generate entry year based on the given index and array of years.
+     *
+     * @param int $index
+     * @param array $years
+     * @return int
+     */
+    private function generateEntryYear(int $index, array $years): int
+    {
+        $entryYearIndex = (int) ($index / 10) % count($years);
+        $entryYear = $years[$entryYearIndex];
+        return $entryYear;
+    }
+
+    /**
+     * Generate student major based on the given index and options.
+     *
+     * @param int $index
+     * @param array $options
+     * @return string
+     */
+    private function generateStudentMajor(int $index, array $options): string
+    {
+        $studentMajorIndex = $index % count($options);
+        $studentMajor = $options[$studentMajorIndex];
+        return $studentMajor;
+    }
+
+    /**
+    * Generate a unique username from the given full name.
+    *
+    * @param string $fullName
+    * @param int $index
+    * @return string
+    */
+    private function generateUsername(string $fullName): string
+    {
+        $username = Str::lower(str_replace(' ', '', $fullName));
+        return $username;
+    }
+    
+
+    /**
+     * Generate an email from the given username.
+     *
+     * @param string $username
+     * @return string
+     */
+    private function generateEmail(string $username): string
+    {
+        $email = $username . '@telkomuniversity.ac.id';
+        return $email;
     }
 }
