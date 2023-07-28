0f29c42914cd189e5e7ed901f09ac258957314f4
anastasiusmanurung <anastasiusmanurung@gmail.com>
2023-07-28 09:47:27 +0700

    fixing core

--- resources/views/scoring/index-after-survey.blade.php
+++ resources/views/scoring/index-after-survey.blade.php
@@ -209,6 +209,7 @@
                                         </ul>
                                     </div>
                                 @endif --}}
+                                @endif
                             </div>
                         @endforeach
                     </div>
